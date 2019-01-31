562311958eafa3aef44d6ff2f1872420e7ad5c83
baptiste <baptiste.name>
2019-01-31 10:50:55 +0100

    Fin index, début des tests unitaires et fonctionnels

---
+++ core/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+class Index(TestCase):
+    def test_uses_index_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
--- core/tests/functional_tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.urls import resolve
-
-from core.views import index
-
-class IndexPageTest(TestCase):
-    pass
