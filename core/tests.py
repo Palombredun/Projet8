01f4cc1fc1e83bedd7f9326d4299c9242c7ee75a
baptiste <baptiste.name>
2019-01-31 11:04:27 +0100

    collectstatic pour régler problème test_uses_index_template()

--- core/tests.py
+++ core/tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 class Index(TestCase):
     def test_uses_index_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'core/index.html')
