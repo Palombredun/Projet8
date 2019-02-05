86c4207477eb76c9b910c73fadfcc7b241db4287
baptiste <baptiste.name>
2019-02-05 16:12:51 +0100

    Test unitaire signup basique fini, factorisation base.html

--- signup/tests.py
+++ signup/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 
 from signup.views import signup
 
@@ -8,3 +9,8 @@ class SignUpTest(TestCase):
     def test_uses_signup_page(self):
         found = resolve('/signup/')
         self.assertEqual(found.func, signup)
+
+    def test_signup_returns_correct_html(self):
+        request = HttpRequest()
+        response = signup(request)
+        self.assertIn(b'<title>Pur Beurre</title>', response.content)
