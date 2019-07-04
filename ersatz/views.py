7fda2ab303c200085fbb37823081447bfe564cae
baptiste <baptiste.name>
2019-07-04 14:06:42 +0200

    collectstatic avant push sur heroku

--- ersatz/views.py
+++ ersatz/views.py
@@ -2,36 +2,42 @@ from difflib import SequenceMatcher
 
 from django.shortcuts import render
 
+from products.models import Product, Category
+
 
 def search(request):
-    pass
-    #if 'query' in request.GET:
-    #    query =  request.GET['query']
-    #    products = Product.objects.filter(product_name__icontains=query)
-    #    if products.count() == 0:
-    #        return render(request, 'ersatz/result.html')
-    #    else:
-    #        # create a list of tuples with the product and its ratio
-    #        # compared to the search of the user, the product with the best
-    #        # ratio will be chosen
-    #        scores = []
-    #        for product in products:
-    #            scores.append((
-    #                product, 
-    #                SequenceMatcher(None, query, product.product_name).ratio())
-    #                )
-    #        scores.sort(key=lambda x: x[1])
-    #        
-    #        product_to_replace = scores[-1][0]
-    #        
-    #        simili_products = Product.objects.filter(
-    #            id_category=product_to_replace.id_category
-    #            ).order_by('nutriscore')
-    #        
-    #        ersatz = []
-    #        for prod in simili_products:
-    #            if prod.nutriscore < product_to_replace.nutriscore:
-    #                ersatz.append(prod)
-    #                
-    #        # return only the top 6 of the products
-    #        return render(request, 'ersatz/result.html', {'products': ersatz[:6]})
+    """
+    If the user's query has not match in the database, simply return the templates.
+    Else, select the product corresponding the most to the query and
+    return the template and up to 6 products with a better nutriscore than the query.
+    """
+    if 'query' in request.GET:
+        query =  request.GET['query']
+        products = Product.objects.filter(product_name__icontains=query)
+
+        if products.count() == 0:
+            return render(request, 'ersatz/result.html')
+        
+        else:
+            foo = []
+            for product in products:
+                # find the best match for the query (highest ratio)
+                foo.append((
+                    product, 
+                    SequenceMatcher(None, query, product.product_name).ratio())
+                    )
+            foo.sort(key=lambda x: x[1])
+            
+            product_to_replace = foo[-1][0]
+            
+            simili_products = Product.objects.filter(
+                id_category=product_to_replace.id_category
+                ).order_by('nutriscore')
+            
+            ersatz = []
+            for prod in simili_products:
+                if prod.nutriscore < product_to_replace.nutriscore:
+                    ersatz.append(prod)
+                    
+            # return only the top 6 of the products
+            return render(request, 'ersatz/result.html', {'products': ersatz[:6]})
