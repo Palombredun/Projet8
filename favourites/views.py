08eb5d23b9cf6a4c990d8e63db5e5796f4a9311f
baptiste <baptiste.name>
2019-07-11 17:46:05 +0200

    retouches mineurs navbar et correction erreur dans result.html

--- favourites/views.py
+++ favourites/views.py
@@ -6,11 +6,9 @@ from products.models import Product
 
 @login_required
 def favourites(request):
+    current_user = {'user': request.user}
     if request.method == 'POST':
-        current_user = {'user': request.user}
         id_product = request.POST['id_product']
         product = Product.objects.filter(id=id_product)
         new_favourite = UserProfile(current_user, product)
-
-
     return render(request, 'favourites/favourites.html', current_user)
